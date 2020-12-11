544ea2c88e7eb01a302a1a459432322e52e573eb
Jimmy Tang <jimmy.tang@equiniti.com>
2020-12-11 16:28:31 +0000

    some fun stuff

---
+++ day4.py
@@ -0,0 +1,25 @@
+import re
+
+fields = {
+    "byr": lambda x: 1920 <= int(x) <= 2002,
+    "iyr": lambda x: 2010 <= int(x) <= 2020,
+    "eyr": lambda x: 2020 <= int(x) <= 2030,
+    "hgt": lambda x: (x.endswith("cm") and 150 <= int(x[:-2]) <= 193) or
+                     (x.endswith("in") and 59 <= int(x[:-2]) <= 76),
+    "hcl": lambda x: re.fullmatch(r"#[\da-f]{6}", x),
+    "ecl": lambda x: x in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth"),
+    "pid": lambda x: re.fullmatch(r"\d{9}", x),
+}
+
+present = 0
+valid = 0
+
+for line in open("input-day4.txt").read().split("\n\n"):
+    passport = dict(l.split(":") for l in line.split())
+    
+    if passport.keys() >= fields.keys():
+        present += 1
+        valid += all(matcherFunction(passport[field]) for field, matcherFunction in fields.items())
+
+print(present)
+print(valid)
